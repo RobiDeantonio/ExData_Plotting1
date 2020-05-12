dc9c80e5f45c4d88b8595b606758fda9972b0493
Robinsson Yesid Sánchez Deantonio <55699603+RobiDeantonio@users.noreply.github.com>
2020-05-12 12:52:43 -0500

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -25,14 +25,26 @@ Data$DateTime <- paste(Data$Date, Data$Time)
 Data$DateTime <- strptime(Data$DateTime, "%Y-%m-%d %H:%M:%S")
 class(Data$DateTime)
 
-#plot3
+#plot4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+
 with(Data, {
-  plot(DateTime, Sub_metering_1, type="l", ylab="Global Active Power (kilowatts)",
-       xlab="", cex=0.8)
-  lines(DateTime,Sub_metering_2, col='Red')
-  lines(DateTime,Sub_metering_3, col='Blue')
+  
+  plot(DateTime, Global_active_power, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="", cex=0.8)
+  
+  plot(DateTime, Voltage, type="l", 
+       ylab="Voltage (volt)", xlab="", cex=0.8)
+  
+  plot(DateTime, Sub_metering_1, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="", cex=0.8)
+  lines(DateTime, Sub_metering_2,col='Red')
+  lines(DateTime, Sub_metering_3,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.5)
+  
+  plot(DateTime, Global_reactive_power, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="", cex=0.8)
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex=0.8)
-dev.copy(png, file="./ExploratoryDataAnalysis/plot3.png", height=480, width=480)
+dev.copy(png, file="./ExploratoryDataAnalysis//plot4.png", height=480, width=480)
 dev.off()
